b99fe4e5cad316c3cbedad3f6e0604344576e69a
TMDNTDL <81274608+TMDNTDL@users.noreply.github.com>
2024-09-08 22:48:47 -0400

    add nav function

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,63 +1,69 @@
 import classNames from 'classnames'
 import './index.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { changeActiveIndex } from '../../store/modules/takeaway'
 
 const Menu = () => {
-  const foodsList = [
-    {
-      "tag": "318569657",
-      "name": "一人套餐",
-      "foods": [
-        {
-          "id": 8078956697,
-          "name": "烤羊肉串(10串)",
-          "like_ratio_desc": "好评度100%",
-          "month_saled": 40,
-          "unit": "10串",
-          "food_tag_list": ["点评网友推荐"],
-          "price": 90,
-          "picture": "https://i.ytimg.com/vi/qD5meFtkT8Q/maxresdefault.jpg",
-          "description": "",
-          "tag": "318569657"
-        },
-        {
-          "id": 7384994864,
-          "name": "腊味煲仔饭",
-          "like_ratio_desc": "好评度81%",
-          "month_saled": 100,
-          "unit": "1人份",
-          "food_tag_list": [],
-          "price": 39,
-          "picture": "https://www.hangheung.com.hk/cdn/shop/articles/a768a8953f9d16a1ab3a8834e2072c8b.jpg?v=1606893667",
-          "description": "",
-          "tag": "318569657"
-        },
-        {
-          "id": 2305772036,
-          "name": "鸡腿胡萝卜焖饭",
-          "like_ratio_desc": "好评度91%",
-          "month_saled": 300,
-          "unit": "1人份",
-          "food_tag_list": [],
-          "price": 34.32,
-          "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCBWX49EzqbiVTyVTxEAsKSzf18V1vaPOjxQ&s",
-          "description": "主料：大米、鸡腿、菜心、胡萝卜",
-          "tag": "318569657"
-        },
-        {
-          "id": 2233861812,
-          "name": "小份酸汤莜面鱼鱼+肉夹馍套餐",
-          "like_ratio_desc": "好评度73%",
-          "month_saled": 600,
-          "unit": "1人份",
-          "food_tag_list": ["“口味好,包装很好～点赞”"],
-          "price": 34.32,
-          "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa3NLgDxVLR7OuHREWBhE5tvqUqamDk0DMbw&s",
-          "description": "酸汤莜面鱼鱼，主料：酸汤、莜面 肉夹馍，主料：白皮饼、猪肉",
-          "tag": "318569657"
-        }
-      ]
-    }
-  ]
+  const { foodsList, activeIndex } = useSelector(state => state.foods)
+  const dispatch = useDispatch()
+  // const foodsList = [
+  //   {
+  //     "tag": "318569657",
+  //     "name": "一人套餐",
+  //     "foods": [
+  //       {
+  //         "id": 8078956697,
+  //         "name": "烤羊肉串(10串)",
+  //         "like_ratio_desc": "好评度100%",
+  //         "month_saled": 40,
+  //         "unit": "10串",
+  //         "food_tag_list": ["点评网友推荐"],
+  //         "price": 90,
+  //         "picture": "https://i.ytimg.com/vi/qD5meFtkT8Q/maxresdefault.jpg",
+  //         "description": "",
+  //         "tag": "318569657"
+  //       },
+  //       {
+  //         "id": 7384994864,
+  //         "name": "腊味煲仔饭",
+  //         "like_ratio_desc": "好评度81%",
+  //         "month_saled": 100,
+  //         "unit": "1人份",
+  //         "food_tag_list": [],
+  //         "price": 39,
+  //         "picture": "https://www.hangheung.com.hk/cdn/shop/articles/a768a8953f9d16a1ab3a8834e2072c8b.jpg?v=1606893667",
+  //         "description": "",
+  //         "tag": "318569657"
+  //       },
+  //       {
+  //         "id": 2305772036,
+  //         "name": "鸡腿胡萝卜焖饭",
+  //         "like_ratio_desc": "好评度91%",
+  //         "month_saled": 300,
+  //         "unit": "1人份",
+  //         "food_tag_list": [],
+  //         "price": 34.32,
+  //         "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCBWX49EzqbiVTyVTxEAsKSzf18V1vaPOjxQ&s",
+  //         "description": "主料：大米、鸡腿、菜心、胡萝卜",
+  //         "tag": "318569657"
+  //       },
+  //       {
+  //         "id": 2233861812,
+  //         "name": "小份酸汤莜面鱼鱼+肉夹馍套餐",
+  //         "like_ratio_desc": "好评度73%",
+  //         "month_saled": 600,
+  //         "unit": "1人份",
+  //         "food_tag_list": ["“口味好,包装很好～点赞”"],
+  //         "price": 34.32,
+  //         "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa3NLgDxVLR7OuHREWBhE5tvqUqamDk0DMbw&s",
+  //         "description": "酸汤莜面鱼鱼，主料：酸汤、莜面 肉夹馍，主料：白皮饼、猪肉",
+  //         "tag": "318569657"
+  //       }
+  //     ]
+  //   }
+  // ]
+  
   const menus = foodsList.map(item => ({ tag: item.tag, name: item.name }))
   return (
     <nav className="list-menu">
@@ -65,10 +71,10 @@ const Menu = () => {
       {menus.map((item, index) => {
         return (
           <div
-            key={item.tag}
+            onClick={()=> dispatch(changeActiveIndex(index))}
             className={classNames(
               'list-menu-item',
-              'active'
+              activeIndex === index && 'active'          
             )}
           >
             {item.name}
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App'
 // injection
 import {Provider} from 'react-redux'
 import store from './store'
+import Menu from './components/Menu'
+import MenuStore from './components/Menu'
 const root = createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
     <App />   
-
+    <Menu />
   </Provider>
   
 )
