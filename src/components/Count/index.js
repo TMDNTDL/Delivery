1f25186a7f604d3dde4508a3c8ec8df2023377d2
TMDNTDL <81274608+TMDNTDL@users.noreply.github.com>
2024-09-16 00:18:49 -0400

    adding the add and sub function in cart

--- src/components/Count/index.js
+++ src/components/Count/index.js
@@ -1,11 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux'
+import {addCart, deleteCart} from '../../store/modules/takeaway'
 import './index.scss'
 
-const Count = ({ onPlus, onMinus, count }) => {
+const Count = ({ onPlus, onMinus, id, count }) => {
+  const carList = useSelector(state=> state.foods)
+  
+  const dispatch = useDispatch()
+  
   return (
     <div className="goods-count">
-      <span className="minus" onClick={onMinus}></span>
+      <span className="minus" onClick={()=>{
+        const item = carList.carList.find(item => item.id === id)
+        dispatch(deleteCart(item))
+      }}></span>
       <span className="count">{count}</span>
-      <span className="plus" onClick={onPlus}></span>
+      <span className="plus" onClick={()=>{
+         const item = carList.carList.find(item => item.id === id)
+         dispatch(addCart(item))
+      }}></span>
     </div>
   )
 }
